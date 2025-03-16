cd3c621dd69f2319cff0b37dd70ce715c23c4683
GitOPEN <gitopen@gmail.com>
2025-03-16 17:10:06 +0800

    change the font size.

--- config/fonts.lua
+++ config/fonts.lua
@@ -5,7 +5,7 @@ local font_with_fallback = {
    { family = 'Sarasa Fixed Slab SC Nerd Font', weight = 'Bold' },
    { family = 'Sarasa Fixed SC Nerd Font',      weight = 'Bold' },
 }
-local font_size = platform().is_mac and 12 or 9
+local font_size = platform().is_mac and 13 or 9
 
 return {
    font = wezterm.font_with_fallback(font_with_fallback),
